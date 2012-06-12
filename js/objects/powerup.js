65a9a60c5f08b06eeecf5bf593a18dbc93239197
Brad Anderson <brad.anderson87@gmail.com>
2012-06-12 17:54:50 -0500

    Just flushing it out a little more

--- js/objects/powerup.js
+++ js/objects/powerup.js
@@ -1,25 +1,61 @@
 var Powerup = Entity.extend({
     type: 'b',
-    width: 10,
-    height: 10,
-    speed: 3,
+    width: 35,
+    height: 65,
+    speed: 2,
+    x: 50,
+    y: 50,
+        
 
     collide: function(object) {
         if (object.player === true) {
             this.kill();
             // set player powerup
             // object.power = ?
+            this.upgrade(object);
         }
     },
+    
     init: function() {
+        
+        this.pauseFlash = new Timer();
+        this.pauseFlash.set(2);
+        
+        this.x = 50; //random (canvas width))
+        
+        this.animSheet = new AnimSheet('shield-sprite.png', 35, 55);
+        this.animFlash = new Anim(this.animSheet, 50, [0, 1, 2, 3, 4, 5, 6, 7], {
+            repeat: false
+        });
+        this.animSet = this.animFlash;
     },
+    
     update: function() {
-        if (this.y > Game.canvas.height) { this.kill(); }
-        else { this.y += this.speed }
+        
+        this._super();
+        
+        if (this.y > Game.canvas.height) {
+            this.kill();
+        } else {
+            this.y += this.speed;
+        }
+
+        if(this.pauseFlash.expire()) {
+            this.animFlash.reset();
+            this.pauseFlash.reset();
+        }
     },
-    draw: function() {
-        // Change to a triangle later
-        Game.ctx.fillStyle = '#ff0000';
-        Game.ctx.fillRect(this.x, this.y, this.width, this.height);
+    
+    upgrade: function(obj) {
+        //obj.health += 3;
+    }
+});
+
+var PowerupShield = Powerup.extend({
+    animSheet: '',
+    offsetX: '',
+    offsetY: '',
+    upgrade: function(obj) {
+        //obj.health += 3;
     }
 });
