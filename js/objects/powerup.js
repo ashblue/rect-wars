16340967548d77637f8ab3fc136eb55de8ca2d9c
unknown <brad.anderson87@gmail.com>
2012-06-17 21:30:02 -0500

    Rewrote the powerups based on game engine changes, added LaBomba

--- js/objects/powerup.js
+++ js/objects/powerup.js
@@ -1,65 +1,68 @@
 /**
  * Powerup
  */
-var Powerup = Entity.extend({
+cp.template.Powerup = cp.template.Entity.extend({
     type: 'b',
     width: 35,
-    height: 65,
-    speed: 2,
-    x: 50,
-    y: 50,
-        
+    height: 55,
+    speed: 1,
+    y: 0,
 
     collide: function(object) {
         if (object.player === true) {
             this.kill();
-            // set player powerup
-            // object.power = ?
             this.upgrade(object);
         }
     },
     
-    init: function() {
-        
-        this.pauseFlash = new Timer();
-        this.pauseFlash.set(2);
-        
-        this.x = 50; //random (canvas width))
-        
-        this.animSheet = new AnimSheet('shield-sprite.png', 35, 55);
-        this.animFlash = new Anim(this.animSheet, 50, [0, 1, 2, 3, 4, 5, 6, 7], {
-            repeat: false
-        });
-        this.animSet = this.animFlash;
-    },
-    
     update: function() {
-        
         this._super();
         
-        if (this.y > Game.canvas.height) {
+        if (this.y > cp.ctx.canvas.height) {
             this.kill();
         } else {
             this.y += this.speed;
         }
-
-        if(this.pauseFlash.expire()) {
-            this.animFlash.reset();
-            this.pauseFlash.reset();
-        }
-    },
-    
-    upgrade: function(obj) {
-        console.log(obj);
     }
 });
 
-var PowerupShield = Powerup.extend({
-    animSheet: '',
-    offsetX: '',
-    offsetY: '',
+cp.template.PowerupShield = cp.template.Powerup.extend({
+
+	init: function() {
+        this.pauseFlash = new cp.timer();
+        this.pauseFlash.set(2);
+        
+        this.x = 50; //random (canvas width))
+        
+        this.animSheet = new cp.animate.sheet('shield-sprite.png', this.width, this.height);
+        this.animFlash = new cp.animate.cycle(this.animSheet, 0.05, [0, 1, 2, 3, 4, 5, 6, 7]);
+        this.animSet = this.animFlash;
+	},
+	
+	update: function() {
+		this._super();
+		if(this.pauseFlash.expire()) {
+            this.animFlash.reset();
+            this.pauseFlash.reset();
+        }
+	},
+	
     upgrade: function(obj) {
-        console.log(obj);
         obj.color = '#00ff00';
     }
+});
+
+cp.template.PowerupLaBomba = cp.template.Powerup.extend({
+	
+	init: function() {
+		this.x = 100;
+		this.animSheet = new cp.animate.sheet('labomba-sprite.png', this.width, this.height);
+		this.animSpin = new cp.animate.cycle(this.animSheet, 0.1, [0, 1, 2, 3]);
+		this.animSpin.repeat = true;
+		this.animSet = this.animSpin;
+	},
+	
+	upgrade: function(obj) {
+		obj.color = '#00f';
+	}
 });
