669e8c928a88629401f0dfc30627d6de54a4e305
Brad Anderson <brad.anderson87@gmail.com>
2012-06-18 10:48:35 -0500

    cleanup

--- js/objects/powerup.js
+++ js/objects/powerup.js
@@ -28,7 +28,7 @@ cp.template.Powerup = cp.template.Entity.extend({
 
 cp.template.PowerupShield = cp.template.Powerup.extend({
 
-	init: function() {
+    init: function() {
         this.pauseFlash = new cp.timer();
         this.pauseFlash.set(2);
         
@@ -37,32 +37,32 @@ cp.template.PowerupShield = cp.template.Powerup.extend({
         this.animSheet = new cp.animate.sheet('shield-sprite.png', this.width, this.height);
         this.animFlash = new cp.animate.cycle(this.animSheet, 0.05, [0, 1, 2, 3, 4, 5, 6, 7]);
         this.animSet = this.animFlash;
-	},
-	
-	update: function() {
-		this._super();
-		if(this.pauseFlash.expire()) {
+    },
+    
+    update: function() {
+        this._super();
+        if(this.pauseFlash.expire()) {
             this.animFlash.reset();
             this.pauseFlash.reset();
         }
-	},
-	
+    },
+    
     upgrade: function(obj) {
         obj.color = '#00ff00';
     }
 });
 
 cp.template.PowerupLaBomba = cp.template.Powerup.extend({
-	
-	init: function() {
-		this.x = 100;
-		this.animSheet = new cp.animate.sheet('labomba-sprite.png', this.width, this.height);
-		this.animSpin = new cp.animate.cycle(this.animSheet, 0.1, [0, 1, 2, 3]);
-		this.animSpin.repeat = true;
-		this.animSet = this.animSpin;
-	},
-	
-	upgrade: function(obj) {
-		obj.color = '#00f';
-	}
+    
+    init: function() {
+        this.x = 100;
+        this.animSheet = new cp.animate.sheet('labomba-sprite.png', this.width, this.height);
+        this.animSpin = new cp.animate.cycle(this.animSheet, 0.1, [0, 1, 2, 3]);
+        this.animSpin.repeat = true;
+        this.animSet = this.animSpin;
+    },
+    
+    upgrade: function(obj) {
+        obj.color = '#00f';
+    }
 });
