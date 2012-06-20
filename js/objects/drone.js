9dd71ee58bb11f232e7b3018c7f4562b23908f18
Gary Koehl <gkoehl@gmail.com>
2012-06-19 20:02:42 -0500

    Updated the player object added in left and right animations.

--- js/objects/drone.js
+++ js/objects/drone.js
@@ -49,7 +49,7 @@ cp.template.Drone = cp.template.Entity.extend({
           break;
         }
     },
-    
+
     draw: function() {
         // Change to a triangle later
         cp.ctx.fillStyle = this.color;
@@ -59,8 +59,9 @@ cp.template.Drone = cp.template.Entity.extend({
 
 cp.template.Drone2 = cp.template.Drone.extend({
     path_points: [],
-    
+
     init: function() {
+        this.path_points = [];
       /* *
       var test_points = [
         [0,350],
@@ -73,43 +74,43 @@ cp.template.Drone2 = cp.template.Drone.extend({
         [200,750],
       ];
       /* */
-      
+
       var min_x = 50;
       var max_x = cp.core.width - min_x;
-      
+
       var next_point = [cp.math.random(min_x, max_x), (-this.height - 5)]; //get the first point
-      
+
       var i = 1;
       var steps = cp.core.height / 100; // move down 100 px at a time
       for (; i <= steps; i++) {
         var current_point = next_point;
         next_point = [cp.math.random(max_x, min_x),(100*i)];
         //next_point = test_points[i];
-        
+
         var diff_x = (current_point[0] - next_point[0]) * -1;
         var diff_y = (current_point[1] - next_point[1]) * -1;
-        
+
         //var distance = Math.sqrt( (diff_x * diff_x) + (diff_y * diff_y));
-        
+
         var rads = Math.atan2(diff_y,diff_x);
 
         var path_step = current_point;
         while(path_step[1] < next_point[1]) {
-          this.path_points.push(path_step);          
+          this.path_points.push(path_step);
           path_step = [
             path_step[0] + this.speed * Math.cos(rads),
             path_step[1] + this.speed * Math.sin(rads)
           ];
         }
       }
-      //console.log(this.path_points);
+      console.log(this.path_points);
     },
     update: function() {
       if (this.path_points.length === 0) {
         this.kill();
         return;
       }
-      
+
       var next_step = this.path_points.shift();
       this.x = next_step[0];
       this.y = next_step[1];
