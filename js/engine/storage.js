683efcb28f6db2159aa85f68b0c6f4afc5473537
Ash Blue <ash@ashbluewebdesign.com>
2012-07-10 20:26:16 -0400

    Removed a leftover console log.

--- js/engine/storage.js
+++ js/engine/storage.js
@@ -12,18 +12,38 @@ Example usage:
 
 - Remove
   cp.storage.remove('name');
-  
+
 - Get
   cp.storage.get('name');
-  
+
 TODO: Might want to consider providing some kind of system crash error in-case
 the user has local storage disabled for some stupid reason.
+
+TODO: Add a cached version of storage as JSON if its faster to access, example:
+    storage: {},
+    build: function() {
+        // Reset current storage
+        this.storage = {};
+
+        // Start loop
+        for (var i = sessionStorage.length; i--;){
+            // Get the storage key
+            var key = sessionStorage.key(i);
+
+            // Get the result
+            var result = this.get(key);
+
+            // Dump gathered data into JSON object
+            this.storage[key] = result;
+        }
+    }
+
 */
 
 var cp = cp || {};
 
-(function(cp, window) {
-    var support = function() {
+(function(cp) {
+    var _support = function() {
         try {
             cp.storage.save('test', 'asdf');
             cp.storage.remove('test');
@@ -32,31 +52,31 @@ var cp = cp || {};
             return false;
         }
     };
-    
+
     cp.storage = {
         init: function() {
-            if (! this.support) {
+            if (! _support) {
                 return alert('Local storage is broken or disabled, please enable it to continue.');
             }
         },
-        
+
         // Saves the passed parameter with a key or tag reference
         save: function(key, value) {
             // Set local storage data internally
             sessionStorage.setItem(key, value);
         },
-        
+
         // Gets select data, should be using build to retrieve
         get: function(key) {
             var result = sessionStorage.getItem(key);
-            
+
             return result;
         },
-        
+
         // Removes the passed paramater
         remove: function(key) {
             // Set local data
             sessionStorage.removeItem(key);
         }
     };
-}(cp, window));
+}(cp));
