73473b9493bb4d4c771cfbcfeacbe5c4fdcac89c
mrmoses <jnmoses@gmail.com>
2012-09-04 20:29:36 -0500

    fixed spacing

--- js/objects/drone-carrier.js
+++ js/objects/drone-carrier.js
@@ -3,7 +3,7 @@ cp.template.DroneCarrier = cp.template.Drone.extend({
     mini_delay: 30,
     mini_delay_count: this.mini_delay,
     minis: 10,
-    
+
     /**
      * options: {
      *   x: initial x
@@ -17,21 +17,21 @@ cp.template.DroneCarrier = cp.template.Drone.extend({
         this.animFrameWidth = 200;
         this.animFrameHeight = 147;
         this.animFrames = [0];
-        
+
         this.speed = 1;
-        
+
         this.x = cp.math.random(cp.core.width, 0);
-        
+
         this.y = -this.animFrameWidth;
-        
+
         this.unload_y = cp.math.random(cp.core.height * 0.25, 50);
 
         options = this._super(options);
     },
-    
+
     update: function() {
       this._super();
-      
+
       if(this.y < this.unload_y) {
         this.y += this.speed;
       } else if(this.minis) {
@@ -42,11 +42,11 @@ cp.template.DroneCarrier = cp.template.Drone.extend({
           }
       }
     },
-    
+
     unloadDroneMini: function() {
         console.log('unload mini');
         cp.game.spawn('DroneMini', {x: this.x, y: this.y, carrier: this})
-        
+
         this.minis -= 1;
         this.mini_delay_count = this.mini_delay;
     }
--- js/objects/drone.js
+++ js/objects/drone.js
@@ -10,31 +10,31 @@ cp.template.Drone = cp.template.Entity.extend({
     hp: 1,
     color: '#CCCCCC',
     accuracy: 0.5, // 0 to 1
-    
+
     init: function(options) {
       options = options || {};
       this.x = options.x || this.x;
       this.y = options.y || this.y;
-      
+
       this.animImage = this.animImage ? this.animImage: 'drone-1.png';
       this.animFrameWidth = this.animFrameWidth ? this.animFrameWidth : 65;
       this.animFrameHeight = this.animFrameHeight ? this.animFrameHeight : 50;
       this.animFrameHeight = this.animFrameHeight ? this.animFrameHeight : 50;
       this.animCycleTime = this.animCycleTime ? this.animCycleTime : 0.1;
       this.animFrames = this.animFrames ? this.animFrames : [0,1,2];
-      
+
       // Create and set an animation sheet (image, frame width, frame height)
       var animSheet = new cp.animate.sheet(this.animImage, this.animFrameWidth, this.animFrameHeight);
 	  //var animSheet = new cp.animate.sheet('drone-2.png', 55, 55);
 	  //var animSheet = new cp.animate.sheet('drone-3.png', 75, 55);
 	  //var animSheet = new cp.animate.sheet('drone-4.png', 95, 55);
-      
+
       this.animDefault = new cp.animate.cycle(animSheet, this.animCycleTime, this.animFrames, false);
-      
+
       this.animSet = this.animDefault;
-      
+
       return options;
-      
+
     },
 
     collide: function(object) {
@@ -45,11 +45,11 @@ cp.template.Drone = cp.template.Entity.extend({
     update: function() {
       this._super();
     },
-    
+
     draw: function() {
         this._super();
     },
-    
+
     fireAtPlayer: function() {
         var players = cp.game.entityGetVal('player',true);
 
@@ -59,25 +59,25 @@ cp.template.Drone = cp.template.Entity.extend({
             //get available targets
             var targets = [];
             for(i in players) {
-            var player = players[i];
-            //if player is in front of drone
-            if(player.y > this.y+50) {
-                targets.push(player);
-            }
+				var player = players[i];
+				//if player is in front of drone
+				if(player.y > this.y+50) {
+					targets.push(player);
+				}
             }
 
             if(targets.length > 0) {
-            //shoot at a random player target
-            var target = targets[cp.math.random(targets.length) - 1];
+				//shoot at a random player target
+				var target = targets[cp.math.random(targets.length) - 1];
 
-            var half_sight_range = (cp.core.width * this.accuracy) / 2;
-            var sight_left = 0 + half_sight_range;
-            var sight_right = cp.core.width - half_sight_range;
+				var half_sight_range = (cp.core.width * this.accuracy) / 2;
+				var sight_left = 0 + half_sight_range;
+				var sight_right = cp.core.width - half_sight_range;
 
-            this.animDefault.reset();
-            //cp.game.spawn('Bullet', this.x, this.y, cp.math.random(sight_right,sight_left), target.y);			
-            cp.game.spawn('Bullet', this.x + 6, this.y + this.height + 4, this.x + 6, cp.core.width + 10);			
-            cp.game.spawn('Bullet', this.x+56, this.y + this.height + 4, this.x+56, cp.core.width + 10);
+				this.animDefault.reset();
+				//cp.game.spawn('Bullet', this.x, this.y, cp.math.random(sight_right,sight_left), target.y);
+				cp.game.spawn('Bullet', this.x + 6, this.y + this.height + 4, this.x + 6, cp.core.width + 10);
+				cp.game.spawn('Bullet', this.x+56, this.y + this.height + 4, this.x+56, cp.core.width + 10);
             }
         }
     }
