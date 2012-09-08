ca1f3e5394cd4eacc919d221e883ae5a1d296d3b
mrmoses <jnmoses@gmail.com>
2012-09-08 10:46:32 -0500

    created level data

--- js/objects/levels/level1.js
+++ js/objects/levels/level1.js
@@ -1,11 +1,12 @@
+/**
+ * @requires models/leveldata.js
+ */
 (function (cp) {
     cp.template.Level1 = cp.template.Entity.extend({
         name: 'level1',
 
         init: function () {
             // Used to determine the width of the game's play area
-            cp.core.gameWidth = 800;
-
             this.bind();
 
             cp.game.spawn('Background');
---
+++ js/objects/models/leveldata.js
@@ -0,0 +1,3 @@
+(function() {
+    cp.core.gameWidth = 800;
+}());
