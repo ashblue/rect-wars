b1e3468d6f80f6e74029ae90dce7e0368fb44e7e
mrmoses <jnmoses@gmail.com>
2012-09-08 11:03:46 -0500

    added private states to drone-dyo

--- js/objects/drone-dyo.js
+++ js/objects/drone-dyo.js
@@ -1,71 +1,104 @@
-cp.template.DroneDyo = cp.template.Drone.extend({
-    next_stop_y: 100,
-    in_position: false,
-    fire_delay: 20,
-    fire_delay_count: 30,
-    clip_size: 3,
-    clip_count: 3,
+(function() {
+    cp.template.DroneDyo = cp.template.Drone.extend({
+        next_stop_y: 100,
+        fire_delay: 20,
+        fire_delay_count: 30,
+        clip_size: 3,
+        clip_count: 3,
 
-    /**
-     * options: {
-     *   x: initial x
-     *   y: initial y
-     *   min_x: farthest point to zig left
-     *   max_x: farthest point to zag right
-     * }
-     */
-    init: function(options) {
-        this.animImage = 'drone-2.png';
-        this.animFrameWidth = 55;
-        this.animFrameHeight = 55;
+        /**
+         * options: {
+         *   x: initial x
+         *   y: initial y
+         *   min_x: farthest point to zig left
+         *   max_x: farthest point to zag right
+         * }
+         */
+        init: function(options) {
+            this.animImage = 'drone-2.png';
+            this.animFrameWidth = 55;
+            this.animFrameHeight = 55;
 
-        //start at random x position
-        this.x = cp.math.random(cp.core.width, 0);
-        this.y = -this.animFrameHeight;
+            //start at random x position
+            this.x = cp.math.random(cp.core.width, 0);
+            this.y = -this.animFrameHeight;
 
-        options = this._super(options);
-    },
+            options = this._super(options);
 
-    update: function() {
-        this._super();
+        },
 
-        if (this.in_position) {
-            if (this.fire_delay_count) {
-                this.fire_delay_count--;
+        update: function() {
+            this._super();
+            _private.update(this);
+        },
+
+        fireAtPlayer: function() {
+            var players = cp.game.entityGetVal('player',true);
+
+            //if there are players to shoot at
+            if(players) {
+                //shoot at a random player target
+                var target = players[cp.math.random(players.length) - 1];
+
+                var half_sight_range = (cp.core.width * this.accuracy) / 2;
+                var sight_left = 0 + half_sight_range;
+                var sight_right = cp.core.width - half_sight_range;
+
+                this.animDefault.reset();
+                cp.game.spawn('Bullet', this.x, this.y, cp.math.random(sight_right, sight_left), target.y);
+            }
+        }
+    });
+
+    var _states = {
+        firing: function(self) {
+            if (self.fire_delay_count) {
+                self.fire_delay_count--;
             } else {
-                if (this.clip_count) {
-                    this.clip_count--;
-                    this.fireAtPlayer();
+                if (self.clip_count) {
+                    self.clip_count--;
+                    self.fireAtPlayer();
                 } else {
-                    this.clip_count = this.clip_size;
-                    this.in_position = false;
+                    // switch to advancing state
+                    _private.changeState(self, 'advancing');
                 }
-                this.fire_delay_count = this.fire_delay;
+                self.fire_delay_count = self.fire_delay;
             }
-        } else {
-            if(this.y < this.next_stop_y) {
-                this.y += this.speed;
-            } else {
-                this.in_position = true;
-                this.next_stop_y += 100;
+        },
+        advancing: function(self) {
+            console.log(self);
+            //if not in position
+            if(self.y < self.next_stop_y) {
+                //continue to move into position
+                self.y += self.speed;
+            } else { //if in position
+                // switch to firing state
+                _private.changeState(self, 'firing');
             }
         }
-    },
+    };
 
-    fireAtPlayer: function() {
-        var players = cp.game.entityGetVal('player',true);
+    var _private = {
+        changeState: function(self, newState) {
+            // switch updates
+            switch(newState){
+                case 'firing':
+                    //reset clip
+                    self.clip_count = self.clip_size;
 
-        //if there are players to shoot at
-        if(players) {
-            //shoot at a random player target
-            var target = players[cp.math.random(players.length) - 1];
+                    // switch update function to this state
+                    this.update = _states.firing;
+                    break;
 
-            var half_sight_range = (cp.core.width * this.accuracy) / 2;
-            var sight_left = 0 + half_sight_range;
-            var sight_right = cp.core.width - half_sight_range;
+                case 'advancing':
+                    // update target position for next advance
+                    self.next_stop_y += 100;
 
-            this.animDefault.reset();
-            cp.game.spawn('Bullet', this.x, this.y, cp.math.random(sight_right, sight_left), target.y);
-        }
-    }
-});
+                    // switch update function to this state
+                    this.update = _states.advancing;
+                    break;
+            }
+        },
+        update: _states.advancing
+    };
+}());
