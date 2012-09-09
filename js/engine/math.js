271ad58ace22b2b63da00e0ff32cd72d65d631a6
mrmoses <jnmoses@gmail.com>
2012-09-08 20:27:44 -0500

    added radian/degree conversions

--- js/engine/math.js
+++ js/engine/math.js
@@ -51,5 +51,25 @@ cp.math = {
         var diff_y = (y1 - y2) * -1;
 
         return Math.sqrt( (diff_x * diff_x) + (diff_y * diff_y));
-    }
+    },
+
+    /**
+     * Convert radians to degree
+     * @link http://stackoverflow.com/questions/135909/is-there-a-built-in-method-for-converting-radians-to-degrees
+     * @param {number} rad Radians to convert
+     * @returns {number} 0 to 360 degrees
+     */
+    radiansToDegrees: function (rad) {
+        return rad * (180 / Math.PI);
+    },
+
+    /**
+     * Convert degrees to radians
+     * @param {number} degrees 0 to 360 degrees
+     * @returns {number} Numerical value of radians
+     */
+    degreesToRadians: function (degrees) {
+        return degrees * (Math.PI / 180);
+    },
+
 };
